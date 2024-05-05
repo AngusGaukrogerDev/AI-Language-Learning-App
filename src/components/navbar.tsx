a9782749e26d801418a65ca9b8ce727dd5807d53
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-05-05 14:35:49 -0500

    Prisma integration!

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: React.FC = () => {
   // console.log("User ID:", userId);
   // console.log("isLoaded:", isLoaded);
   return (
-    <nav className="w-full h-16 bg-pitahaya-light-grey flex flex-row justify-around items-center py-5 z-[98] top-0">
+    <nav className="w-full h-20 bg-pitahaya-light-grey flex flex-row justify-around items-center py-5 z-[98] top-0">
         <HomeIcon />
         <div className="flex flex-row justify-center items-center gap-3">
           <ClerkUserHeader />
