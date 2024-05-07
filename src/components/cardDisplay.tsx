cb9b8a781e925d7e8b1bdec0dda7a4ca940d9f7b
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-05-07 09:32:47 -0500

    Making basic words show up

--- src/components/cardDisplay.tsx
+++ src/components/cardDisplay.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
-interface CardDisplayProps {
-  id: string; // Assuming id is of type number, adjust as needed
+interface Word {
+  id: number;
+  englishTranslation: string;
+  spanishTranslation: string;
 }
 
-const CardDisplay: React.FC<CardDisplayProps> = ({ id }) => {
+interface CardDisplayProps {
+  words: Word[];
+}
+
+const CardDisplay: React.FC<CardDisplayProps> = ({ words }) => {
   return (
     <div className="lg:h-3/4 lg:w-1/4 gap-2 md:gap-5 justify-center items-center bg-pitahaya-white rounded-md">
-      {/* You can use the id prop here */}
-      <p>ID: {id}</p>
+      {words.map((word) => (
+        <div key={word.id}>
+          <p>ID: {word.id}</p>
+          <p>English Translation: {word.englishTranslation}</p>
+          <p>Spanish Translation: {word.spanishTranslation}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/lessons/[id]/index.tsx
+++ src/pages/lessons/[id]/index.tsx
@@ -1,9 +1,20 @@
 import { useRouter } from 'next/router';
 import Navbar from "@/components/navbar";
 import CardDisplay from '@/components/cardDisplay';
+import { GetServerSideProps, InferGetServerSidePropsType} from "next";
+import { prisma } from "@/lib/prisma";
 import React from 'react';
 
-const LessonScreen: React.FC = () => {
+type Word = {
+  id: number;
+  englishTranslation: string;
+  spanishTranslation: string;
+};
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+
+const LessonScreen: React.FC<Props> = ({ nextWords }: Props) => {
   const router = useRouter();
   // Check if router.query.id exists before accessing its value
   const lessonId = router.query.id ? router.query.id.toString() : '';
@@ -12,10 +23,40 @@ const LessonScreen: React.FC = () => {
     <>
       <Navbar />
       <div className="w-full h-screen flex flex-col justify-center items-center gap-3 bg-pitahaya-light-grey">
-        <CardDisplay id={lessonId} />
+        <CardDisplay words={nextWords} />
       </div>
     </>
   );
 };
 
 export default LessonScreen;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const { id } = context.query;
+    if (!id || typeof id !== "string") {
+      throw new Error("Invalid lesson ID");
+    }
+    
+    const lessonId = parseInt(id);
+
+    const words: Word[] = await prisma.word.findMany({
+      where: {
+        lessonId: lessonId
+      }
+    });
+    return {
+      props: {
+        nextWords: words,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching next word:", error);
+    // Return an empty array if an error occurs
+    return {
+      props: {
+        nextWords: [],
+      },
+    };
+  }
+}
