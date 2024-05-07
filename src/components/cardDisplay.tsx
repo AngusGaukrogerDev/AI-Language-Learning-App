cd86ed17e7571932196adc38991358a941075ad4
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-05-07 16:16:31 -0500

    Sorting json into 1 obj

--- src/components/cardDisplay.tsx
+++ src/components/cardDisplay.tsx
@@ -19,20 +19,32 @@ interface CardDisplayProps {
 }
 
 const CardDisplay: React.FC<CardDisplayProps> = ({ words, userWordsProgress }) => {
+  // Function to merge word and user progress
+  const mergeObjects = (words: Word[], userWordsProgress: UserWordsProgress[]): any[] => {
+    const mergedObjects: any[] = [];
+
+    words.forEach(word => {
+      const matchingProgress = userWordsProgress.find(progress => progress.wordId === word.id);
+      if (matchingProgress) {
+        mergedObjects.push({ ...word, ...matchingProgress });
+      }
+    });
+
+    return mergedObjects;
+  };
+
+  const mergedData = mergeObjects(words, userWordsProgress);
+
   return (
-    <div className="lg:h-1/2 lg:w-1/4 gap-2 md:gap-5 justify-center items-center bg-pitahaya-white rounded-md">
-      {words.map((word) => (
-        <div key={word.id}>
-          <p>ID: {word.id}</p>
-          <p>English Translation: {word.englishTranslation}</p>
-          <p>Spanish Translation: {word.spanishTranslation}</p>
-        </div>
-      ))}
-      {userWordsProgress.map((userWordProgress) => (
-        <div key={userWordProgress.id}>
-          <p>WordID: {userWordProgress.wordId}</p>
-          <p>UserID: {userWordProgress.userId}</p>
-          <p>Word Level: {userWordProgress.wordLevel}</p>
+    <div className="lg:h-auto lg:w-1/4 gap-2 md:gap-5 justify-center items-center bg-pitahaya-white rounded-md">
+      {mergedData.map((item) => (
+        <div className='my-1' key={item.id}>
+          <p>ID: {item.id}</p>
+          <p>English Translation: {item.englishTranslation}</p>
+          <p>Spanish Translation: {item.spanishTranslation}</p>
+          <p>WordID: {item.wordId}</p>
+          <p>UserID: {item.userId}</p>
+          <p>Word Level: {item.wordLevel}</p>
         </div>
       ))}
     </div>
