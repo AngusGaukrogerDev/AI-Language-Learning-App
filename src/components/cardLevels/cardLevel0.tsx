a5aa5115c9f2cc5599ad8900e9dc8c2e5944dbd4
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-05-08 17:40:17 -0500

    restyle lvl-0

--- src/components/cardLevels/cardLevel0.tsx
+++ src/components/cardLevels/cardLevel0.tsx
@@ -16,17 +16,16 @@ interface CardLevel0Props {
 
 const CardLevel0: React.FC<CardLevel0Props> = ({ cardData, onIndexChange }) => {
   const handleIncrement = () => {
-    // Call the parent component's callback to increment the index
     onIndexChange(true);
   };
 
   return (
     <>
-      <div className='my-1 bg-pitahaya-grey border-white text-white' key={cardData.id}>
-        <img src="" alt="" />
-        <h3>{cardData.englishTranslation}</h3>
-        <h3>{cardData.spanishTranslation}</h3>
-        <button onClick={handleIncrement}>Next</button> {/* Button triggers increment */}
+      <div className="bg-gray-100 border border-gray-200 rounded-lg p-4 mb-4">
+        <img src="" alt="" className="mb-2" />
+        <h3 className="text-xl font-semibold mb-2">{cardData.englishTranslation}</h3>
+        <h3 className="text-xl mb-2">{cardData.spanishTranslation}</h3>
+        <button onClick={handleIncrement} className="bg-pitahaya-yellow hover:bg-pitahaya-black text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Next</button> {/* Button triggers increment */}
       </div>
     </>
   );
